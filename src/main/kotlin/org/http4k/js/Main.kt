9b0adcc1a9af779a23bc902cec4a3ed75a2f901d
Ivan Sanchez <s4nchez@gmail.com>
2017-09-29 14:51:14 +0100

    just promise

--- src/main/kotlin/org/http4k/js/Main.kt
+++ src/main/kotlin/org/http4k/js/Main.kt
@@ -1,10 +1,5 @@
 package org.http4k.js
 
-import kotlin.coroutines.experimental.Continuation
-import kotlin.coroutines.experimental.CoroutineContext
-import kotlin.coroutines.experimental.EmptyCoroutineContext
-import kotlin.coroutines.experimental.startCoroutine
-import kotlin.coroutines.experimental.suspendCoroutine
 import kotlin.js.Promise
 
 external class XMLHttpRequest {
@@ -20,24 +15,9 @@ external class XMLHttpRequest {
 fun main(args: Array<String>) {
     println("Hello JavaScript! Let's make an async request")
 
-    launch {
-        val response: XMLHttpRequest = makeRequest("GET", "http://httpbin.org/get").await()
-        println("response (${response.status}) = ${response.responseText}")
-    }
-}
-
-suspend fun <T> Promise<T>.await(): T = suspendCoroutine { cont ->
-    then({ cont.resume(it) }, { cont.resumeWithException(it) })
-}
-
-fun launch(block: suspend () -> Unit) {
-    block.startCoroutine(object : Continuation<Unit> {
-        override val context: CoroutineContext get() = EmptyCoroutineContext
-        override fun resume(value: Unit) {}
-        override fun resumeWithException(e: Throwable) {
-            console.log("Coroutine failed: $e")
-        }
-    })
+        makeRequest("GET", "http://httpbin.org/get").then({ response ->
+            println("response (${response.status}) = ${response.responseText}")
+        })
 }
 
 fun makeRequest(method: String, url: String): Promise<XMLHttpRequest> = Promise { resolve, _ ->
