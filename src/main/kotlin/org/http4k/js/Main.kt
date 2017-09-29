ad62fe86f05a547ea4e6df7b1b41b9558e4939e0
Ivan Sanchez <s4nchez@gmail.com>
2017-09-29 11:19:17 +0100

    Make async request

--- src/main/kotlin/org/http4k/js/Main.kt
+++ src/main/kotlin/org/http4k/js/Main.kt
@@ -1,33 +1,50 @@
 package org.http4k.js
 
-/*
-var request = new XMLHttpRequest();
-request.open('GET', '/bar/foo.txt', false);  // `false` makes the request synchronous
-request.send(null);
-
-if (request.status === 200) {
-  console.log(request.responseText);
-}
- */
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.EmptyCoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.experimental.suspendCoroutine
+import kotlin.js.Promise
 
 external class XMLHttpRequest {
-    fun open(method: String, uri: String, sync: Boolean)
-    fun send(data: Any?)
+    fun open(method: String, uri: String)
+    fun send()
+    fun addEventListener(name: String, callback: (something: Any) -> Unit)
 
     var status: Int
     var responseText: String
 }
 
+
 fun main(args: Array<String>) {
-    println("Hello JavaScript! Let's make a request")
+    println("Hello JavaScript! Let's make an async request")
 
-    val request = XMLHttpRequest()
-    request.open("GET", "http://httpbin.org/get", false)
-    request.send(null)
-
-
-    if (request.status == 200) {
-        println("got ${request.responseText}")
+    launch {
+        val response: XMLHttpRequest = makeRequest("GET", "http://httpbin.org/get").await()
+        println("response (${response.status}) = ${response.responseText}")
     }
+}
 
+suspend fun <T> Promise<T>.await(): T = suspendCoroutine { cont ->
+    then({ cont.resume(it) }, { cont.resumeWithException(it) })
+}
+
+fun launch(block: suspend () -> Unit) {
+    block.startCoroutine(object : Continuation<Unit> {
+        override val context: CoroutineContext get() = EmptyCoroutineContext
+        override fun resume(value: Unit) {}
+        override fun resumeWithException(e: Throwable) {
+            console.log("Coroutine failed: $e")
+        }
+    })
+}
+
+fun makeRequest(method: String, url: String): Promise<XMLHttpRequest> = Promise { resolve, _ ->
+    val xhr = XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.addEventListener("load", { e ->
+        resolve(xhr)
+    })
+    xhr.send()
 }
